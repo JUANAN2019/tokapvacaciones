c6bb51ec54794698b46b07a501c9eefec6ac624b
JUANAN2019 <garciagalindo@telefonica.net>
2023-09-05 09:18:35 +0200

    test funcionando panther pero crawler no lee variable

--- tests/ArtistTest.php
+++ tests/ArtistTest.php
@@ -1,16 +1,50 @@
 <?php
 
-namespace App\Tests;
-
 use Symfony\Component\Panther\PantherTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use App\Entity\Artist;
 
 class ArtistTest extends PantherTestCase
 {
-    public function testSomething(): void
+    public function testSetName()
     {
         $client = static::createPantherClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/artist');
 
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $name = "John Doe";
+        $artist = new Artist();
+        $artist->setName($name);
+
+        $this->assertEquals($name, $artist->getName());
     }
+
+    public function testSetCountry()
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/artist');
+
+        $country = "United States";
+        $artist = new Artist();
+        $artist->setCountry($country);
+
+        $this->assertEquals($country, $artist->getCountry());
+    }
+
+    // Agrega más métodos de prueba para las demás propiedades y funcionalidades de la clase Artist
+
+    // ...
 }
+// namespace App\Tests;
+
+// use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+// use Symfony\Component\DomCrawler\Crawler;
+
+// class ArtistTest extends WebTestCase
+// {
+//     public function testSomething(): void
+//     {
+//         $client = static::createClient();
+//         $crawler = $client->request('GET', '/api/artist');
+//         $this->assertSelectorTextContains('h1', 'Hello World');
+//     }
+// }
